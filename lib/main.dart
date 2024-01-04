9ff9c7a761c867196e00a5a66562220c6c392e09
Connection || Peter <69660530+Connection2Peter@users.noreply.github.com>
2024-01-04 11:46:27 +0800

    v240104_1146
    Popup function

--- lib/main.dart
+++ lib/main.dart
@@ -35,14 +35,53 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
-        child: FittedBox(
-          fit: BoxFit.fitWidth,
-          child: Image.asset(
-            'assets/華光三部曲_final_page-0005.jpg',
-            width: 100.w,
-            height: 100.h,
+        child: GestureDetector(
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return popUpImage('莊子心性', 'assets/莊子心性-人本無情.jpg');
+              },
+            );
+          },
+          child: FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Image.asset(
+              'assets/華光三部曲_final_page-0005.jpg',
+              width: 100.w,
+              height: 100.h,
+            ),
           ),
         ),
+      ),
+    );
+  }
+
+  AlertDialog popUpImage(String imageName, path2Image) {
+    return AlertDialog(
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(imageName),
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(Icons.close),
+          ),
+        ],
+      ),
+      content: StatefulBuilder(
+        builder: (BuildContext context, StateSetter setAtomState) {
+          return FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Image.asset(
+              path2Image,
+              width: 80.w,
+              height: 80.h,
+            ),
+          );
+        },
       ),
     );
   }
