d3b6e2f61c7408fcaaf388c44a4931f0ea7a0216
Connection <69660530+ConnectionOuOb@users.noreply.github.com>
2024-03-27 23:29:16 +0800

    Reformate project arch

---
+++ lib/object.dart
@@ -0,0 +1,14 @@
+class PopImage {
+  String imageName;
+  String path2Image;
+  double x, y, width, height;
+
+  PopImage(
+    this.imageName,
+    this.path2Image,
+    this.x,
+    this.y,
+    this.width,
+    this.height,
+  );
+}
